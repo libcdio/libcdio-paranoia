a500a7037729aaeaba9bfda9c007d598e8fa8adc
Adrian Reber <adrian@lisas.de>
2012-11-03 11:37:56 +0100

    Fixup license headers
    After the libcdio - libcdio-paranoia split many license headers
    were wrong. The original cdparanoia library is LGPLv2+ and the
    binaries are GPLv2+. This changes all GPLv3 headers from the
    libcdio split back to GPLv2+ and the files in lib/paranoia to
    LGPLv2+.

--- src/pod2c.pl
+++ src/pod2c.pl
@@ -18,9 +18,9 @@ open(STDIN, "<$filename")
 print "/*
   Copyright (C) 1999, 2005, 2007, 2008 Rocky Bernstein
    
-  This program is free software: you can redistribute it and/or modify
+  This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
+  the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
@@ -28,8 +28,9 @@ print "/*
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 static const char ${name}_help[] =\n";
 while(<STDIN>) {
