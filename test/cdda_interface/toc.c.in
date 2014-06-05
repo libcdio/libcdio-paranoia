libcdio/libcdio-paranoia

commit c94fcc7b041f872d3ffd5ba2642c27ba6cfabe70

Alighn paranoia's TOC functions with cdio's. bug #38421 (can't handle a CD-ROM with 99 tracks). More tests should be added.

lib/cdda_interface/toc.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2005, 2008 Rocky Bernstein <rocky@gnu.org>
+  Copyright (C) 2005, 2008, 2013 Rocky Bernstein <rocky@gnu.org>
   Copyright (C) 1998-2008 Monty xiphmont@mit.edu
   derived from code (C) 1994-1996 Heiko Eissfeldt
 
@@ -29,33 +29,33 @@
 #include "low_interface.h"
 #include "utils.h"
 
-/*! Return the lsn for the start of track i_track */
+/*! Return the lsn for the start of track i_track or CDIO_LEADOUT_TRACK */
 lsn_t
 cdda_track_firstsector(cdrom_drive_t *d, track_t i_track)
 {
   if(!d->opened){
     cderror(d,"400: Device not open\n");
     return(-400);
-  }
-
-  if (i_track == 0) {
-    if (d->disc_toc[0].dwStartSector == 0) {
-      /* first track starts at lsn 0 -> no pre-gap */
-      cderror(d,"401: Invalid track number\n");
-      return(-401);
-    }
-    else {
-      return 0; /* pre-gap of first track always starts at lba 0 */
-    }
-  }
-
-  if( i_track>d->tracks) {
-    cderror(d,"401: Invalid track number\n");
-    return(-401);
-  }
-
-  {
+  } else {
     const track_t i_first_track = cdio_get_first_track_num(d->p_cdio);
+    const track_t i_last_track  =
+	cdio_get_last_track_num(d->p_cdio) + 1; /* include leadout */
+
+    if (i_track == CDIO_CDROM_LEADOUT_TRACK) i_track = i_last_track;
+    if (i_track < i_first_track || i_track > i_last_track) {
+      char buf[100];
+      snprintf(buf, sizeof(buf), "401: Invalid track number %02d\n", i_track);
+      cderror(d, buf);
+      return(-401);
+    } else if (i_track == i_first_track) {
+      if (d->disc_toc[0].dwStartSector != 0) {
+	/* first track starts at lsn 0 -> no pre-gap */
+	cderror(d,"402: No initial pregap\n");
+	return(-401);
+    } else {
+	return 0; /* pre-gap of first track always starts at lba 0 */
+      }
+    }
     return(d->disc_toc[i_track-i_first_track].dwStartSector);
   }
 }
@@ -79,7 +79,7 @@ cdda_disc_firstsector(cdrom_drive_t *d)
        return cdda_track_firstsector(d, i+1);
     }
 
-  cderror(d,"403: No audio tracks on disc\n");  
+  cderror(d,"403: No audio tracks on disc\n");
   return(-403);
 }
 
@@ -108,7 +108,7 @@ cdda_track_lastsector(cdrom_drive_t *d, track_t i_track)
     cderror(d,"401: Invalid track number\n");
     return -401;
   }
-  
+
   /* CD Extra have their first session ending at the last audio track */
   if (d->cd_extra > 0 && i_track+1 <= d->tracks) {
     if (d->audio_last_sector >= d->disc_toc[i_track-1].dwStartSector &&
@@ -138,17 +138,17 @@ cdda_disc_lastsector(cdrom_drive_t *d)
       if ( cdda_track_audiop(d,i) )
 	return (cdda_track_lastsector(d,i));
   }
-  cderror(d,"403: No audio tracks on disc\n");  
+  cderror(d,"403: No audio tracks on disc\n");
   return -403;
 }
 
-/*! Return the number of tracks on the CD. */
+/*! Return the number of tracks on the or 300 if error. */
 track_t
 cdda_tracks(cdrom_drive_t *d)
 {
   if (!d->opened){
     cderror(d,"400: Device not open\n");
-    return -400;
+    return CDIO_INVALID_TRACK;
   }
   return(d->tracks);
 }
@@ -158,15 +158,16 @@ cdda_tracks(cdrom_drive_t *d)
     error or the LSN after the LEADOUT (beyond the end of the CD), then
     CDIO_INVALID_TRACK is returned.
  */
-int 
+int
 cdda_sector_gettrack(cdrom_drive_t *d, lsn_t lsn)
 {
   if (!d->opened) {
     cderror(d,"400: Device not open\n");
-    return -400;
+    return CDIO_INVALID_TRACK;
   } else {
-    if (lsn < d->disc_toc[0].dwStartSector)
-      return 0; /* We're in the pre-gap of first track */
+    if (lsn <= d->disc_toc[0].dwStartSector)
+      /* We're in the start of or pre-gap of first track */
+      return cdio_get_first_track_num(d->p_cdio);
 
     return cdio_get_track(d->p_cdio, lsn);
   }
@@ -176,14 +177,14 @@ cdda_sector_gettrack(cdrom_drive_t *d, lsn_t lsn)
   implemented or -1 for error.
   Not meaningful if track is not an audio track.
 */
-int 
+int
 cdda_track_channels(cdrom_drive_t *d, track_t i_track)
 {
   return(cdio_get_track_channels(d->p_cdio, i_track));
 }
 
 /*! Return 1 is track is an audio track, 0 otherwise. */
-int 
+int
 cdda_track_audiop(cdrom_drive_t *d, track_t i_track)
 {
   track_format_t track_format = cdio_get_track_format(d->p_cdio, i_track);
@@ -191,20 +192,19 @@ cdda_track_audiop(cdrom_drive_t *d, track_t i_track)
 }
 
 /*! Return 1 is track is an audio track, 0 otherwise. */
-int 
+int
 cdda_track_copyp(cdrom_drive_t *d, track_t i_track)
 {
   track_flag_t track_flag = cdio_get_track_copy_permit(d->p_cdio, i_track);
   return CDIO_TRACK_FLAG_TRUE == track_flag ? 1 : 0;
 }
 
-/*! Return 1 is audio track has linear preemphasis set, 0 otherwise. 
+/*! Return 1 is audio track has linear preemphasis set, 0 otherwise.
     Only makes sense for audio tracks.
  */
-int 
+int
 cdda_track_preemp(cdrom_drive_t *d, track_t i_track)
 {
   track_flag_t track_flag = cdio_get_track_preemphasis(d->p_cdio, i_track);
   return CDIO_TRACK_FLAG_TRUE == track_flag ? 1 : 0;
 }
-

test/cdda_interface/toc.c.in
@@ -1,0 +1,124 @@
+/*
+  Copyright (C) 2013 Rocky Bernstein <rocky@gnu.org>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+   Regression test for lib/cdda_interface/toc.c
+
+   To compile as s standalone program:
+
+ gcc -g3 -Wall -DHAVE_CONFIG_H -I../.. -I../..include toc.c \
+      ../../lib/paranoia/libcdio_paranoia.la ../../lib/cdda_interface/.lib/libcdio_cdda.a -lcdio -o toc
+
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#define __CDIO_CONFIG_H__ 1
+#endif
+
+#ifdef HAVE_STDIO_H
+#include <stdio.h>
+#endif
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include <cdio/paranoia/cdda.h>
+#include <cdio/cd_types.h>
+#include <cdio/logging.h>
+
+#ifndef DATA_DIR
+#define DATA_DIR "@srcdir@/../data"
+#endif
+
+static void
+log_handler (cdio_log_level_t level, const char message[])
+{
+  switch(level) {
+  case CDIO_LOG_DEBUG:
+  case CDIO_LOG_INFO:
+    return;
+  default:
+    printf("cdio %d message: %s\n", level, message);
+  }
+}
+
+static int testit(cdrom_drive_t *d) {
+    lsn_t lsn = cdda_disc_lastsector(d);
+    track_t t = cdda_tracks(d);
+
+    if (t != 1) {
+	printf("Should have found one track, got %d\n", t);
+	return 1;
+    }
+
+    if ( lsn != 301 ) {
+	printf("Should have gotten 301 as the last sector, got %d.\n", lsn);
+	return 2;
+    }
+
+    t = cdda_sector_gettrack(d, 100);
+    if ( 1 != t )  {
+	printf("Should have gotten 1 as the track, got %d.\n", t);
+	return 2;
+    }
+
+    t = cdda_sector_gettrack(d, 1000);
+    if ( CDIO_INVALID_TRACK != t )  {
+	printf("Should have gotten CDIO_INVALID_TRACK as the track, got %d.\n", t);
+	return 3;
+    }
+
+    return 0;
+}
+
+int
+main(int argc, const char *argv[])
+{
+  cdrom_drive_t *d = NULL; /* Place to store handle given by cd-paranoia. */
+  CdIo_t *p_cdio;
+  int rc;
+
+  cdio_log_set_handler (log_handler);
+
+  if (cdio_have_driver(DRIVER_BINCUE)) {
+      p_cdio = cdio_open("@native_abs_top_srcdir@/test/data/cdda.cue", DRIVER_UNKNOWN);
+  } else if (cdio_have_driver(DRIVER_CDRDAO)) {
+      p_cdio = cdio_open("@native_abs_top_srcdir@/test/data/cdda.toc", DRIVER_UNKNOWN);
+  } else {
+    printf("-- You don't have enough drivers for this test\n");
+    return 77;
+  }
+
+  d=cdio_cddap_identify_cdio(p_cdio, 1, NULL);
+
+  if ( !d ) {
+    printf("Should have identified as an audio CD disc.\n");
+    return 1;
+  }
+
+  if ( 0 != cdio_cddap_open(d) ) {
+    printf("Unable to open disc.\n");
+    return 2;
+  }
+
+  rc = testit(d);
+  cdio_cddap_close_no_free_cdio(d);
+  cdio_destroy(p_cdio);
+
+  return rc;
+}

.gitignore
@@ -1,4 +1,4 @@
-/*~
+*~
 /ChangeLog
 /Makefile
 /Makefile.in
@@ -24,3 +24,5 @@
 /ltmain.sh
 /missing
 /stamp-h1
+/*.directive
+/*.directive.asc

configure.ac
@@ -1,4 +1,4 @@
-dnl Copyright (C)  2011, 2012
+dnl Copyright (C)  2011-2013
 dnl Rocky Bernstein <rocky@gnu.org>
 dnl
 dnl This program is free software; you can redistribute it and/or modify
@@ -26,7 +26,7 @@ AC_PREREQ(2.67)
 
 AC_INIT([libcdio-paranoia], [LIBCDIO_PARANOIA_VERSION_STR(PARANOIA_VERSION,RELEASE_NUM)], [libcdio-help@gnu.org])
 AC_CONFIG_SRCDIR(src/cd-paranoia.c)
-AM_INIT_AUTOMAKE
+AM_INIT_AUTOMAKE([foreign])
 
 AC_CANONICAL_HOST
 AM_CONFIG_HEADER(config.h)
@@ -65,7 +65,7 @@ AC_ARG_ENABLE(example-progs,
 AM_CONDITIONAL(BUILD_EXAMPLES, test "x$enable_example_progs" != "xno")
 AM_CONDITIONAL(BUILD_VERSIONED_LIBS, test "x$enable_versioned_libs" = "xyes")
 
-dnl We use C 
+dnl We use C
 AC_PROG_CC
 AM_PROG_CC_C_O
 dnl We also use C++ in example programs and for CXX bindings
@@ -74,7 +74,7 @@ AC_PROG_CXX
 dnl Checks for programs.
 AC_AIX
 
-if test "x$GCC" != "xyes" 
+if test "x$GCC" != "xyes"
 then
   AC_MSG_WARN([
   *** non GNU CC compiler detected.
@@ -100,7 +100,7 @@ else
   unset warning_flags
 fi
 
-PKG_CHECK_MODULES(LIBCDIO, libcdio >= 0.83, [], 
+PKG_CHECK_MODULES(LIBCDIO, libcdio >= 0.83, [],
 	[AC_MSG_ERROR(Required libcdio library not found. Please get libcdio from http://www.gnu.org/software/libcdio/ and install it.)])
 AC_SUBST(LIBCDIO_LIBS)
 AC_SUBST(LIBCDIO_CFLAGS)
@@ -116,10 +116,10 @@ DIFF_OPTS=
 
 if test "$DIFF" = no ; then
    AC_PATH_PROG(DIFF, cmp, no)
-else 
+else
   # Try for GNU diff options.
   # MSDOG output uses \r\n rather than \n in tests
-  for diff_opt in -w --unified ; do 
+  for diff_opt in -w --unified ; do
     if $DIFF $diff_opt ./configure ./configure > /dev/null 2>&1; then
       AC_MSG_RESULT([adding $diff_opt to diff in regression tests])
       DIFF_OPTS="$DIFF_OPTS $diff_opt"
@@ -144,7 +144,7 @@ AC_CHECK_HEADERS(stdarg.h stdbool.h stdio.h sys/cdio.h sys/param.h \
 		 sys/time.h sys/timeb.h sys/utsname.h)
 
 # FreeBSD 4 has getopt in unistd.h. So we include that before
-# getopt.h 
+# getopt.h
 AC_CHECK_HEADERS(unistd.h getopt.h)
 
 AC_SUBST(SBPCD_H)
@@ -158,7 +158,7 @@ AC_C_INLINE
 dnl ISOC99_PRAGMA
 AC_MSG_CHECKING([whether $CC supports ISOC99 _Pragma()])
 AC_TRY_COMPILE([],[_Pragma("pack(1)")], [
-  ISOC99_PRAGMA=yes 
+  ISOC99_PRAGMA=yes
   AC_DEFINE(HAVE_ISOC99_PRAGMA, [], [Supports ISO _Pragma() macro])
 ],ISOC99_PRAGMA=no)
 AC_MSG_RESULT($ISOC99_PRAGMA)
@@ -207,7 +207,7 @@ if test "x$empty_array_size" = "xxxx"
 then
   AC_MSG_ERROR([compiler is unable to creaty empty arrays])
 else
-  AC_DEFINE_UNQUOTED(EMPTY_ARRAY_SIZE, $empty_array_size, 
+  AC_DEFINE_UNQUOTED(EMPTY_ARRAY_SIZE, $empty_array_size,
 	[what to put between the brackets for empty arrays])
   changequote(`,')
   msg="[${empty_array_size}]"
@@ -222,6 +222,18 @@ dnl AM_PROG_LIBTOOL tests whether we have GNU ld
 dnl this must come before checking --with-versioned-libs
 dnl which requires GNU ld.
 AM_PROG_LIBTOOL
+
+dnl regression tests need native-style absolute paths.
+dnl native_abs_top_srcdir is used here.
+case $srcdir in
+  .)  # We are building in place.
+   native_abs_top_srcdir=$ac_pwd ;;
+  [\\/]* | ?:[\\/]* )  # Absolute name.
+    native_abs_top_srcdir=$srcdir ;;
+  *) # Relative name.
+    native_abs_top_srcdir=$ac_pwd/$srcdir ;;
+esac
+AC_SUBST(native_abs_top_srcdir)
 
 dnl system
 
@@ -238,17 +250,17 @@ if test "$with_gnu_ld" != yes; then
    enable_versioned_libs='no'
 fi
 # We also need GNU make to build versioned symbols.
-if test "x$enable_versioned_libs" = "xyes" ; then 
+if test "x$enable_versioned_libs" = "xyes" ; then
   if test -n "$MAKE" ; then
     $MAKE --version 2>/dev/null >/dev/null
     if test "$?" -ne 0 ; then
-       AC_MSG_ERROR(Either set MAKE variable to GNU make or use 
+       AC_MSG_ERROR(Either set MAKE variable to GNU make or use
    --without-versioned-libs option)
     fi
-  else 
+  else
     make --version 2>/dev/null >/dev/null
     if test "$?" -ne 0 ; then
-       AC_MSG_ERROR(Either set MAKE variable to GNU make or use 
+       AC_MSG_ERROR(Either set MAKE variable to GNU make or use
    --without-versioned-libs option)
     fi
   fi
@@ -257,7 +269,7 @@ fi
 AM_CONDITIONAL(CYGWIN, test "x$CYGWIN" = "xyes")
 
 dnl Checks for header files.
-     
+
 LIBCDIO_CDDA_LIBS='$(top_builddir)/lib/cdda_interface/libcdio_cdda.la'
 LIBCDIO_PARANOIA_CFLAGS='-I$(top_builddir)/include -I$(top_srcdir)/include/'
 LIBCDIO_PARANOIA_LIBS='$(top_builddir)/lib/paranoia/libcdio_paranoia.la'
@@ -276,9 +288,9 @@ case $host_os in
        ##     [Define 1 if you have AIX CD-ROM support])
        ;;
      darwin6*|darwin7*|darwin8*|darwin9*)
-       AC_CHECK_HEADERS(IOKit/IOKitLib.h CoreFoundation/CFBase.h, 
+       AC_CHECK_HEADERS(IOKit/IOKitLib.h CoreFoundation/CFBase.h,
                         [have_iokit_h="yes"])
-       if test "x$have_iokit_h" = "xyes" ; then 
+       if test "x$have_iokit_h" = "xyes" ; then
          AC_DEFINE([HAVE_DARWIN_CDROM], [1],
 	      [Define 1 if you have Darwin OS X-type CD-ROM support])
 	 DARWIN_PKG_LIB_HACK="-Wl,-framework,CoreFoundation -Wl,-framework,IOKit"
@@ -309,9 +321,9 @@ case $host_os in
 #include <linux/cdrom.h>
 struct cdrom_generic_command test;
 int has_timeout=sizeof(test.timeout);],
-            [AC_DEFINE([HAVE_LINUX_CDROM_TIMEOUT], [1], 
+            [AC_DEFINE([HAVE_LINUX_CDROM_TIMEOUT], [1],
                    [Define 1 if timeout is in cdrom_generic_command struct])])
-	   AC_DEFINE([HAVE_LINUX_CDROM], [1], 
+	   AC_DEFINE([HAVE_LINUX_CDROM], [1],
                      [Define 1 if you have Linux-type CD-ROM support])
 	   cd_drivers="${cd_drivers}, GNU/Linux"
 	fi
@@ -319,7 +331,7 @@ int has_timeout=sizeof(test.timeout);],
      bsdi*)
         AC_CHECK_HEADERS(dvd.h, [have_bsdi_dvd_h="yes"])
 	if test "x$have_bsdi_dvd_h" = "xyes" ; then
-     	  AC_DEFINE([HAVE_BSDI_CDROM], [1], 
+     	  AC_DEFINE([HAVE_BSDI_CDROM], [1],
                     [Define 1 if you have BSDI-type CD-ROM support])
 	 LIBS="$LIBS -ldvd -lcdrom"
          LIBCDIO_LIBS="$LIBCDIO_LIBS  -lcdrom"
@@ -327,40 +339,40 @@ int has_timeout=sizeof(test.timeout);],
 	fi
 	;;
      sunos*|sun*|solaris*)
-     	AC_DEFINE([HAVE_SOLARIS_CDROM], [1], 
+     	AC_DEFINE([HAVE_SOLARIS_CDROM], [1],
                   [Define 1 if you have Solaris CD-ROM support])
 	  cd_drivers="${cd_drivers}, Solaris"
 	;;
      cygwin*)
-	AC_DEFINE([CYGWIN], [1], 
+	AC_DEFINE([CYGWIN], [1],
                   [Define 1 if you are compiling using cygwin])
-     	AC_DEFINE([HAVE_WIN32_CDROM], [1], 
+     	AC_DEFINE([HAVE_WIN32_CDROM], [1],
                   [Define 1 if you have MinGW CD-ROM support])
  	LIBS="$LIBS -lwinmm"
  	LT_NO_UNDEFINED="-no-undefined"
   	cd_drivers="${cd_drivers}, MinGW"
-     	AC_DEFINE([NEED_TIMEZONEVAR], [1], 
+     	AC_DEFINE([NEED_TIMEZONEVAR], [1],
                   [Define 1 if you need timezone defined to get timzone
 		   defined as a variable. In cygwin it is a function too])
 	;;
      mingw*)
 	AC_CHECK_HEADERS(windows.h)
-	AC_DEFINE([MINGW32], [1], 
+	AC_DEFINE([MINGW32], [1],
                   [Define 1 if you are compiling using MinGW])
-     	AC_DEFINE([HAVE_WIN32_CDROM], [1], 
+     	AC_DEFINE([HAVE_WIN32_CDROM], [1],
                   [Define 1 if you have MinGW CD-ROM support])
  	LIBS="$LIBS -lwinmm -mwindows"
  	LT_NO_UNDEFINED="-no-undefined"
   	cd_drivers="${cd_drivers}, MinGW "
 	;;
      freebsd4.*|freebsd5.*|freebsd6*|freebsd7*|freebsd8*|dragonfly*|kfreebsd*)
-     	AC_DEFINE([HAVE_FREEBSD_CDROM], [1], 
+     	AC_DEFINE([HAVE_FREEBSD_CDROM], [1],
                   [Define 1 if you have FreeBSD CD-ROM support])
  	LIBS="$LIBS -lcam"
   	cd_drivers="${cd_drivers}, FreeBSD "
 	;;
      netbsd*)
-     	AC_DEFINE([HAVE_NETBSD_CDROM], [1], 
+     	AC_DEFINE([HAVE_NETBSD_CDROM], [1],
                   [Define 1 if you have NetBSD CD-ROM support])
  	# LIBS="$LIBS -lcam"
   	cd_drivers="${cd_drivers}, NetBSD "
@@ -422,6 +434,8 @@ AC_CONFIG_FILES([
        src/Makefile \
        src/usage.txt \
        test/data/Makefile
+       test/cdda_interface/Makefile
+       test/cdda_interface/toc.c
        test/Makefile
        ])
 

doc/doxygen/.gitignore
@@ -2,4 +2,5 @@
 /Doxyfile.bak
 /html
 /latex
+/libcdio-paranoia.xml
 /libcdio.xml

commit 276dac1e82e3bcbb1b57021108865eec628104c0

lib/cdda_interface/toc.c: fixed pregap and first track handling.
It was broken in commit c94fcc7b04... which removed special handling of track 0 (representing the pregap/hidden track one) always returning error -401 in that case. Instead, the pregap code was moved to a block handling the first track (track 1).

Testing can be further improved by adding tests on images with pregap.

lib/cdda_interface/toc.c
@@ -42,19 +42,20 @@ cdda_track_firstsector(cdrom_drive_t *d, track_t i_track)
 	cdio_get_last_track_num(d->p_cdio) + 1; /* include leadout */
 
     if (i_track == CDIO_CDROM_LEADOUT_TRACK) i_track = i_last_track;
-    if (i_track < i_first_track || i_track > i_last_track) {
+    if (i_track == 0) {
+      if (d->disc_toc[0].dwStartSector == 0) {
+	/* first track starts at lba 0 -> no pre-gap */
+	cderror(d,"402: No initial pregap\n");
+	return(-402);
+      }
+      else {
+	return 0; /* pre-gap of first track always starts at lba 0 */
+      }
+    } else if (i_track < i_first_track || i_track > i_last_track) {
       char buf[100];
       snprintf(buf, sizeof(buf), "401: Invalid track number %02d\n", i_track);
       cderror(d, buf);
       return(-401);
-    } else if (i_track == i_first_track) {
-      if (d->disc_toc[0].dwStartSector != 0) {
-	/* first track starts at lsn 0 -> no pre-gap */
-	cderror(d,"402: No initial pregap\n");
-	return(-401);
-    } else {
-	return 0; /* pre-gap of first track always starts at lba 0 */
-      }
     }
     return(d->disc_toc[i_track-i_first_track].dwStartSector);
   }
@@ -88,37 +89,40 @@ cdda_disc_firstsector(cdrom_drive_t *d)
 lsn_t
 cdda_track_lastsector(cdrom_drive_t *d, track_t i_track)
 {
-  if (!d->opened) {
+  if(!d->opened){
     cderror(d,"400: Device not open\n");
-    return -400;
-  }
+    return(-400);
+  } else {
+    const track_t i_first_track = cdio_get_first_track_num(d->p_cdio);
+    const track_t i_last_track  = cdio_get_last_track_num(d->p_cdio);
 
-  if (i_track == 0) {
-    if (d->disc_toc[0].dwStartSector == 0) {
-      /* first track starts at lba 0 -> no pre-gap */
-      cderror(d,"401: Invalid track number\n");
+    if (i_track == 0) {
+      if (d->disc_toc[0].dwStartSector == 0) {
+	/* first track starts at lba 0 -> no pre-gap */
+	cderror(d,"402: No initial pregap\n");
+	return(-402);
+      }
+      else {
+	return d->disc_toc[0].dwStartSector-1;
+      }
+    } else if (i_track < i_first_track || i_track > i_last_track) {
+      char buf[100];
+      snprintf(buf, sizeof(buf), "401: Invalid track number %02d\n", i_track);
+      cderror(d, buf);
       return(-401);
     }
-    else {
-      return d->disc_toc[0].dwStartSector-1;
+
+    /* CD Extra have their first session ending at the last audio track */
+    if (d->cd_extra > 0 && i_track-i_first_track+2 <= d->tracks) {
+      if (d->audio_last_sector >= d->disc_toc[i_track-i_first_track].dwStartSector &&
+          d->audio_last_sector < d->disc_toc[i_track-i_first_track+1].dwStartSector) {
+        return d->audio_last_sector;
+      }
     }
-  }
 
-  if ( i_track<1 || i_track>d->tracks ) {
-    cderror(d,"401: Invalid track number\n");
-    return -401;
+    /* Safe, we've always the leadout at disc_toc[tracks] */
+    return(d->disc_toc[i_track-i_first_track+1].dwStartSector-1);
   }
-
-  /* CD Extra have their first session ending at the last audio track */
-  if (d->cd_extra > 0 && i_track+1 <= d->tracks) {
-    if (d->audio_last_sector >= d->disc_toc[i_track-1].dwStartSector &&
-        d->audio_last_sector < d->disc_toc[i_track].dwStartSector) {
-      return d->audio_last_sector;
-    }
-  }
-
-  /* Safe, we've always the leadout at disc_toc[tracks] */
-  return(d->disc_toc[i_track].dwStartSector-1);
 }
 
 /*! Get last lsn of the last audio track. The last lssn generally one
@@ -165,9 +169,8 @@ cdda_sector_gettrack(cdrom_drive_t *d, lsn_t lsn)
     cderror(d,"400: Device not open\n");
     return CDIO_INVALID_TRACK;
   } else {
-    if (lsn <= d->disc_toc[0].dwStartSector)
-      /* We're in the start of or pre-gap of first track */
-      return cdio_get_first_track_num(d->p_cdio);
+    if (lsn < d->disc_toc[0].dwStartSector)
+      return 0; /* We're in the pre-gap of first track */
 
     return cdio_get_track(d->p_cdio, lsn);
   }

test/cdda_interface/toc.c.in
@@ -1,5 +1,6 @@
 /*
   Copyright (C) 2013 Rocky Bernstein <rocky@gnu.org>
+  Copyright (C) 2014 Robert Kausch <robert.kausch@freac.org>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -58,19 +59,61 @@ log_handler (cdio_log_level_t level, const char message[])
 }
 
 static int testit(cdrom_drive_t *d) {
-    lsn_t lsn = cdda_disc_lastsector(d);
-    track_t t = cdda_tracks(d);
+    track_t t = 0;
+    lsn_t lsn = 0;
 
+    /* Check number of tracks.
+     */
+    t = cdda_tracks(d);
     if (t != 1) {
 	printf("Should have found one track, got %d\n", t);
 	return 1;
     }
 
+    /* Check disc sector reporting.
+     */
+    lsn = cdda_disc_firstsector(d);
+    if ( lsn != 0 ) {
+	printf("Should have gotten 0 as the first sector, got %d.\n", lsn);
+	return 2;
+    }
+
+    lsn = cdda_disc_lastsector(d);
     if ( lsn != 301 ) {
 	printf("Should have gotten 301 as the last sector, got %d.\n", lsn);
 	return 2;
     }
 
+    /* Check pregap sector reporting (should fail here).
+     */
+    lsn = cdda_track_firstsector(d, 0);
+    if ( lsn != -402 ) {
+	printf("Should have gotten error code -402, got %d.\n", lsn);
+	return 3;
+    }
+
+    lsn = cdda_track_lastsector(d, 0);
+    if ( lsn != -402 ) {
+	printf("Should have gotten error code -402, got %d.\n", lsn);
+	return 3;
+    }
+
+    /* Check track sector reporting.
+     */
+    lsn  = cdda_track_firstsector(d, 1);
+    if ( lsn != 0 ) {
+	printf("Should have gotten 0 as the first sector, got %d.\n", lsn);
+	return 2;
+    }
+
+    lsn  = cdda_track_lastsector(d, 1);
+    if ( lsn != 301 ) {
+	printf("Should have gotten 301 as the last sector, got %d.\n", lsn);
+	return 2;
+    }
+
+    /* Check sector track reporting.
+     */
     t = cdda_sector_gettrack(d, 100);
     if ( 1 != t )  {
 	printf("Should have gotten 1 as the track, got %d.\n", t);