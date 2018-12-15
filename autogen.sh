3d1fb61cd5474026f947666f44c7ad0d156747c6
Edd Barrett <vext01@gmail.com>
2018-12-15 18:26:53 +0000

    OpenBSD: Make autogen.sh work.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,10 @@
 # Run this to generate all the initial makefiles, etc.
 # Additional options go to configure.
 
+# For OpenBSD
+export AUTOCONF_VERSION=2.69
+export AUTOMAKE_VERSION=1.15
+
 echo "Rebuilding ./configure with autoreconf..."
 autoreconf -f -i
 if [ $? -ne 0 ]; then
